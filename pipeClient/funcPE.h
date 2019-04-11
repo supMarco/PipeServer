3970ff2d0979b498e4cc2b44edb48403a5231d4d
Marco <sublimemarco@outlook.it>
2019-04-11 15:05:55 +0200

    tiny updt #16

--- pipeClient/funcPE.h
+++ pipeClient/funcPE.h
@@ -4,4 +4,4 @@
 WORD get_machine_type(BYTE *pe);
 DWORD get_time_date_stamp(BYTE *pe);
 DWORD get_exported_functions(BYTE *dll, BYTE **names, WORD machine);
-DWORD rva_to_file_offset_x64_x86(BYTE *dll, DWORD rva, WORD machine);
+DWORD rva_to_file_offset(BYTE *dll, DWORD rva, WORD machine);
